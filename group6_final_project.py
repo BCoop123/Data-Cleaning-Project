ed0ee43667ab23801f9c54086c77912fa9e2da17
BCoop123 <brandoncooper859@gmail.com>
2023-11-30 14:36:33 -0500

    remove menu

--- group6_final_project.py
+++ group6_final_project.py
@@ -20,38 +20,9 @@ import pandas as pd
 # define functions
 #========================================================================
 
-def displayMenu():
-    print("\n" + "=" * 46)
-    print("|" + " " * 20 + "Menu" + " " * 20 + "|")
-    print("=" * 46)
-    print(f"{'| 1. Merge Datasets':<45}" + "|")
-    print(f"{'| 2. Clean Lab6 Data':<45}" + "|")
-    print(f"{'| 3. Exit':<45}" + "|")
-    print("=" * 46 + "\n")
+def getData():
+    # Get CSV data
+    data = pd.read_csv('../../Datasets/AP4_PTec_Coils.csv')
+    print(data.head())
 
-    option = -1
-
-    #ensures that a valid option is chosen
-    while (option < 1) or (option > 3):
-        try:
-            option = int(input("Please select an option from the menu (1-3): "))
-            if (option < 1) or (option > 3):
-                print("Invalid Input!")
-        except:
-            print("Invalid Input!")
-
-    #option that quits the program
-    if (option == 3):
-        return True
-
-    #executes user chosen option
-    if (option == 1):
-        mergeDatasets()
-    if (option == 2):
-        cleanData()
-
-    return False
-
-# Get CSV data
-data = pd.read_csv('../../Datasets/AP4_PTec_Coils.csv')
-print(data.head())
+getData()

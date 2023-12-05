3fac1e1cde7842606137ca7f680bd518e8157223
BCoop123 <brandoncooper859@gmail.com>
2023-12-05 14:44:05 -0500

    error handling and db connection

--- group6_final_project.py
+++ group6_final_project.py
@@ -15,31 +15,65 @@
 #========================================================================
 
 import pandas as pd
+import os
+import configparser as cp
 
 #========================================================================
 # define functions
 #========================================================================
 
+# Check if Datasets folder exists before running
+flag = True
+url = ""
+dbuser = ""
+dbpassword = ""
+
+if not os.path.exists('./Datasets'):
+    flag = False
+    print("Need to create a folder named Datasets.")
+
+if not os.path.exists('./db.conf'):
+    flag = False
+    print("Need to create a file name db.conf with credentials.")
+else:
+    try:
+        confP = cp.ConfigParser()
+
+        # read in the ini or configuration file
+        confP.read(["db.conf"])
+
+        url = confP.get("db", "url")
+        dbuser = confP.get("db", "dbuser")
+        dbpassword = confP.get("db", "dbpassword")
+        
+
+    except:
+        flag = False
+        print("The db.conf file is configured incorrectly. See README.md for configuration.")
+
 # Get the data contained in all of the CSV files used for this project
 def getData():
     # Get CSV data
-    pTechCoilsData = pd.read_csv('./Datasets/AP4_PTec_Coils.csv')
-    pTechDefectMapsData = pd.read_csv('./Datasets/AP4_Ptec_Defect_Maps_10-coils.csv')
-    claimsData = pd.read_csv('./Datasets/claims_2023-05.csv')
-    flInspectionCommentsData = pd.read_csv('./Datasets/FLInspectionComments.csv')
-    flInspectionMappedDefectsData = pd.read_csv('./Datasets/tblFlatInspectionMappedDefects.csv')
-    flInspectionProcessesData = pd.read_csv('./Datasets/tblFlatInspectionProcesses.csv')
-    flInspectionData = pd.read_csv('./Datasets/tblFLInspection.csv')
+    try:
+        pTechCoilsData = pd.read_csv('./Datasets/AP4_PTec_Coils.csv')
+        pTechDefectMapsData = pd.read_csv('./Datasets/AP4_Ptec_Defect_Maps_10-coils.csv')
+        claimsData = pd.read_csv('./Datasets/claims_2023-05.csv')
+        flInspectionCommentsData = pd.read_csv('./Datasets/FLInspectionComments.csv')
+        flInspectionMappedDefectsData = pd.read_csv('./Datasets/tblFlatInspectionMappedDefects.csv')
+        flInspectionProcessesData = pd.read_csv('./Datasets/tblFlatInspectionProcesses.csv')
+        flInspectionData = pd.read_csv('./Datasets/tblFLInspection.csv')
 
-    return [
-        pTechCoilsData,
-        pTechDefectMapsData,
-        claimsData,
-        flInspectionCommentsData,
-        flInspectionMappedDefectsData,
-        flInspectionProcessesData,
-        flInspectionData
-    ]
+        return [
+            pTechCoilsData,
+            pTechDefectMapsData,
+            claimsData,
+            flInspectionCommentsData,
+            flInspectionMappedDefectsData,
+            flInspectionProcessesData,
+            flInspectionData
+        ]
+    except:
+        print("Failed to read files.")
 
 # Clean data that is stored in a dataframe
 def cleanData(dataframe):
@@ -49,14 +83,19 @@ def cleanData(dataframe):
 
 # Merge two datasets given the names, joinCondition, and joinType
 def mergeDatasets(dataset1, dataset2, joinCondition, joinType):
-    df = pd.merge({}, {}, on='{}', how='{}').format(dataset1, dataset2, joinCondition, joinType)
-
-    return df
+    try:
+        df = pd.merge({}, {}, on='{}', how='{}').format(dataset1, dataset2, joinCondition, joinType)
+        return df
+    except:
+        print("Failed to merge datasets.")
 
 #========================================================================
 # main program
 #========================================================================
 
-dataframeList = getData()
+if flag:
 
-print(dataframeList[0].head())
+    dataframeList = getData()
+    
+    print(dataframeList[0].head())
+    print(url)
